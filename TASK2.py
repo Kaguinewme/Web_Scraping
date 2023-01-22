b2751baa75e96b5af67fc370a9a7492ebf76ee42
Kaguinewme <95537815+Kaguinewme@users.noreply.github.com>
2023-01-22 18:04:40 +0530

    Add files via upload

---
+++ TASK2.py
@@ -0,0 +1,18 @@
+import json
+file=open("movies.json")
+movies=json.load(file)
+def group_by_year():
+    dic={}
+    for i in movies:
+        movie_list=[]
+        year=i["year"]
+        if year not in dic:
+            for j in movies:
+                if year==j["year"]:
+                    movie_list.append(j)
+            dic[year]=movie_list
+        print(dic)
+    with open("TASK2.json","w") as f:
+        json.dump(dic,f,indent=8,sort_keys=True)
+    # print(file)
+group_by_year()
